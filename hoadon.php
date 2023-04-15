ca9f60fc363669f6213433125de85984a9f14514
Akuro <luongluuminhtan@gmail.com>
2023-04-15 22:27:06 +0700

    Xong Hoa Don

--- hoadon.php
+++ hoadon.php
@@ -19,13 +19,13 @@
         <div class="row">
           <div class="col-4">
             <div class="card">
-            
-              <?php 
+
+              <?php
               if (!isset($_SESSION['email']) || empty($_SESSION['email'])) {
-                  // Nếu chưa đăng nhập, hiển thị thông báo hoặc chuyển hướng người dùng đến trang khác
-                  echo "<script>alert('Bạn cần đăng nhập để truy cập vào trang này.');</script>";
-                  echo "<script>window.location.href='index.php';</script>";
-                  exit;
+                // Nếu chưa đăng nhập, hiển thị thông báo hoặc chuyển hướng người dùng đến trang khác
+                echo "<script>alert('Bạn cần đăng nhập để truy cập vào trang này.');</script>";
+                echo "<script>window.location.href='index.php';</script>";
+                exit;
               } else {
                 $tongkh = "SELECT SUM(tuyenxe.GIAHIENHANH) AS TongTien
                 FROM vexe
@@ -35,7 +35,7 @@
                 INNER JOIN tuyenxe ON tuyenxe.ID_TUYEN = chuyenxe.ID_TUYEN
                 WHERE khachhang.email='" . $_SESSION["email"] . "'
                 ";
-    $result = mysqli_query($conn, $tongkh);
+                $result = mysqli_query($conn, $tongkh);
               }
 
               $tong1 = $result->fetch_assoc();
@@ -53,9 +53,9 @@
                 <hr class="horizontal dark my-2">
                 <h5 class="mb-0 ">
                   <?php
-                                    $number = $tong1["TongTien"];
-                                    $formatted_number = number_format($number, 3);
-                                    echo $formatted_number ?> đồng
+                  $number = $tong1["TongTien"];
+                  $formatted_number = number_format($number, 3);
+                  echo $formatted_number ?> đồng
                 </h5>
               </div>
             </div>
@@ -112,26 +112,19 @@
                   <table id="myTable" class="display" class="table align-items-center mb-0">
                     <thead>
                       <tr class="col-12">
-                      <th
-                          class="col-2 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                        <th class="col-2 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
                           Tên phiếu</th>
-                          <th
-                          class="col-1 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                        <th class="col-1 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
                           Số vé</th>
-                        <th
-                          class="col-3 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                        <th class="col-3 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                           Tên chuyến xe</th>
-                        <th
-                          class="col-2 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                        <th class="col-2 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
                           Thời điểm đi</th>
-                        <th
-                          class="col-2 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                        <th class="col-2 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
                           Ngày lập hóa đơn</th>
-                          <th
-                          class="col-1 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                        <th class="col-1 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
                           Giá</th>
-                          <th
-                          class="col-1 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                        <th class="col-1 text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
                         </th>
                       </tr>
                     </thead>
@@ -147,9 +140,7 @@
                               INNER JOIN chuyenxe ON vexe.ID_CHUYENXE = chuyenxe.ID_CHUYENXE
                               INNER JOIN tuyenxe ON tuyenxe.ID_TUYEN = chuyenxe.ID_TUYEN
                               WHERE khachhang.email='" . $_SESSION["email"] . "'
-                                                                  
-                                    
-                                    
+                              GROUP BY chuyenxe.TENCHUYENXE;     
                                     ";
                       //thuc thi cau lenh sql va dua doi tuong vao $result
                       $result = $conn->query($sql);
@@ -161,54 +152,109 @@
                         $result_all = $result->fetch_all();
 
                         foreach ($result_all as $row) {
+                          $constrain_del = "SELECT pdv.*, vx.* FROM phieudatve pdv
+                                  INNER JOIN vexe vx ON pdv.MAPHIEU = vx.MAPHIEU
+                                  INNER JOIN chuyenxe cx ON vx.ID_CHUYENXE = cx.ID_CHUYENXE
+                                  WHERE pdv.MAPHIEU = '" . $row[0] . "'
+                                  AND cx.THOIDIEMDITT < NOW()"; // Thêm điều kiện kiểm tra thời gian đi của chuyến xe
+                          $result_constrain_del = mysqli_query($conn, $constrain_del);
+                          if (!$result_constrain_del) {
+                            echo "Hủy chuyến thất bại: " . $conn->error;
+                          } else {
+                            $numRows = mysqli_num_rows($result_constrain_del);
+                            if ($numRows > 0) {
                       ?>
-                      <form action="xoahoadon.php" method="post">
-                        <tr>
-                          <td class="align-middle text-center">
-                            <!-- ma hd -->
-                            <?php echo $row[0];
-                              echo '<input type="text" name="idphieu" value="'.$row[0].'" style="display: none;">';
-                            ?>
-                          </td>
-                          <td class="align-middle text-center">
-                            <!-- ma hd -->
-                            <?php echo $row[5] ?>
-                          </td>
-                          <td class="align-middle text-center">
-                            <!-- ngayhoanthanh -->
-                            <?php echo $row[1] ?>
-                          </td>
-                          <td class="align-middle text-center">
-                            <!-- soluong -->
-                            <?php echo $row[2] ?>
-                          </td>
-                          <td class="align-middle text-xs text-center">
-                            <?php echo $row[3] ?>
-                          </td>
-                          <!-- phuong thuc thanh toan -->
-                          <td class="align-middle text-xs text-center">
-  
+                              <tr>
+                                <td class="align-middle text-center text-muted">
+                                  <!-- ma hd -->
+                                  <?php echo $row[0];
+                                  echo '<input type="text" name="idphieu" value="' . $row[0] . '" style="display: none;">';
+                                  ?>
+                                </td>
+                                <td class="align-middle text-center text-muted">
+                                  <!-- ma hd -->
+                                  <?php echo $row[5] ?>
+                                </td>
+                                <td class="align-middle text-center text-muted">
+                                  <!-- ngayhoanthanh -->
+                                  <?php echo $row[1] ?>
+                                </td>
+                                <td class="align-middle text-center text-muted">
+                                  <!-- soluong -->
+                                  <?php echo $row[2] ?>
+                                </td>
+                                <td class="align-middle text-xs text-center text-muted">
+                                  <?php echo $row[3] ?>
+                                </td>
+                                <!-- phuong thuc thanh toan -->
+                                <td class="align-middle text-xs text-center text-muted">
+
+                                  <?php
+
+                                  $number = $row[4] * $row[5];
+                                  $formatted_number = number_format($number, 3);
+                                  echo $formatted_number . "đ";
+                                  ?>
+
+                                </td>
+                                <td class="align-middle text-xs text-center text-muted">
+                                </td>
+
+                                <td class="align-middle text-success text-center text-muted">
+                                </td>
+                              </tr>
                             <?php
-  
-                                $number = $row[4]*$row[5];
-                                $formatted_number = number_format($number, 3);
-                                echo $formatted_number. "đ";
-                                ?>
-  
-                          </td>
-                          <td class="align-middle text-xs text-center">
-                            <button type="submit" name="action" value="xoaChuyen" class="text-danger border-0 bg-transparent" style="cursor: pointer;"></i></i><b style="font-size: large;">☒</b></button>
-                          </td>
-  
-                          <td class="align-middle text-success text-center">
-                            <!-- tongtien -->
-  
-  
-  
-                          </td>
-                        </tr>
-                      </form>
+                            } else {
+                            ?>
+                              <form action="xoahoadon.php" method="post">
+                                <tr>
+                                  <td class="align-middle text-center">
+                                    <!-- ma hd -->
+                                    <?php echo $row[0];
+                                    echo '<input type="text" name="idphieu" value="' . $row[0] . '" style="display: none;">';
+                                    ?>
+                                  </td>
+                                  <td class="align-middle text-center">
+                                    <!-- ma hd -->
+                                    <?php echo $row[5] ?>
+                                  </td>
+                                  <td class="align-middle text-center">
+                                    <!-- ngayhoanthanh -->
+                                    <?php echo $row[1] ?>
+                                  </td>
+                                  <td class="align-middle text-center">
+                                    <!-- soluong -->
+                                    <?php echo $row[2] ?>
+                                  </td>
+                                  <td class="align-middle text-xs text-center">
+                                    <?php echo $row[3] ?>
+                                  </td>
+                                  <!-- phuong thuc thanh toan -->
+                                  <td class="align-middle text-xs text-center">
+
+                                    <?php
+
+                                    $number = $row[4] * $row[5];
+                                    $formatted_number = number_format($number, 3);
+                                    echo $formatted_number . "đ";
+                                    ?>
+
+                                  </td>
+                                  <td class="align-middle text-xs text-center">
+                                    <button type="submit" name="action" value="xoaChuyen" class="text-danger border-0 bg-transparent" style="cursor: pointer;"></i></i><b style="font-size: large;"><i class="fa-solid fa-trash-can"></i></b></button>
+                                  </td>
+
+                                  <td class="align-middle text-success text-center">
+                                    <!-- tongtien -->
+
+
+
+                                  </td>
+                                </tr>
+                              </form>
                       <?php
+                            }
+                          }
                         }
                       }
                       $tongkh = "SELECT COUNT(ID_CHUYENXE) AS TONGVE FROM chuyenxe";
@@ -223,8 +269,8 @@
                         <td>
                         </td>
                         <div style="margin-top: 10px;">
-                        <td></td>
-                        <td></td>
+                          <td></td>
+                          <td></td>
                           <td style="text-align: right">
                             <br>
                             <b> Tổng tiền: <b>
@@ -235,7 +281,7 @@
                             <?php
                             $number = $tongve;
                             $formatted_number = number_format($number, 3);
-                            echo $formatted_number."đ";
+                            echo $formatted_number . "đ";
                             ?>
                           </td>
                         </div>
@@ -266,15 +312,14 @@
               <div class="col-2 text-center me-n3">
                 <button id="btnIn" class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i class="fas fa-print text-sm me-1"></i>
                   In</button>
-                  <script>
-document.getElementById("btnIn").addEventListener("click", function() {
-    window.print(); // Gọi hàm in của trình duyệt
-});
-</script>
+                <script>
+                  document.getElementById("btnIn").addEventListener("click", function() {
+                    window.print(); // Gọi hàm in của trình duyệt
+                  });
+                </script>
               </div>
               <div class="col-3 text-center">
-                <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-1"><i
-                    class="fas fa-file-pdf text-sm me-1"></i> Xuất PDF</button>
+                <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-1"><i class="fas fa-file-pdf text-sm me-1"></i> Xuất PDF</button>
               </div>
             </div>
           </div>
--- xoahoadon.php
+++ xoahoadon.php
@@ -8,22 +8,6 @@ if (!$conn) {
 $idPDV = $_POST['idphieu'];
 
 if ($_POST['action'] == 'xoaChuyen') {
-    $query = "SELECT pdv.*, vx.* FROM phieudatve pdv
-              INNER JOIN vexe vx ON pdv.MAPHIEU = vx.MAPHIEU
-              INNER JOIN chuyenxe cx ON vx.ID_CHUYENXE = cx.ID_CHUYENXE
-              WHERE pdv.MAPHIEU = '".$idPDV."'
-              AND cx.THOIDIEMDITT < NOW()"; // Thêm điều kiện kiểm tra thời gian đi của chuyến xe
-    $result = mysqli_query($conn, $query);
-    if (!$result) {
-      echo "Hủy chuyến thất bại: " . $conn->error;
-    } else {
-      $numRows = mysqli_num_rows($result);
-      if ($numRows > 0) {
-        echo '<script language="javascript">
-        alert("Không thể hủy chuyến vì đã quá thời gian đi!");
-        history.back();
-        </script>';
-      } else {
         $rmVe = "DELETE FROM vexe WHERE MAPHIEU = '".$idPDV."'";
         $result = mysqli_query($conn, $rmVe);
         $rmPDV = "DELETE FROM phieudatve WHERE MAPHIEU = '".$idPDV."'";
@@ -36,7 +20,4 @@ if ($_POST['action'] == 'xoaChuyen') {
           history.back();
           </script>';
         }
-      }
-    }
 }
-?>
